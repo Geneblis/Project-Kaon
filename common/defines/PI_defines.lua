34f8e686d0a3265b5de0c701ca195460ef23e507
Geneblis <Gbyelo@protonmail.com>
2024-01-23 17:09:20 -0300

    DAAAAAAAAMN

--- common/defines/PI_defines.lua
+++ common/defines/PI_defines.lua
@@ -1,3 +1,3 @@
-NDefines.NGame.START_DATE = "5.1.1.1"
-NDefines.NGame.END_DATE = "25.12.31.1"
+NDefines.NGame.START_DATE = "522.1.1.1"
+NDefines.NGame.END_DATE = "545.12.31.1"
 NDefines.NGame.HANDS_OFF_START_TAG = "AUT"
